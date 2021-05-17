79851f518cdab7bd7576e59040c966e54b1266df
RRoager <rasmus-roager@hotmail.com>
2021-05-17 12:13:41 +0200

    Inflowscreen project oprettet

---
+++ src/main/java/gruppe10/inflowscreen/FrontendController.java
@@ -0,0 +1,62 @@
+package gruppe10.inflowscreen;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("{orgName}")
+@CrossOrigin(value = "*")
+public class FrontendController {
+
+
+    @GetMapping("")
+    public String index(@PathVariable String orgName, Model model){
+        model.addAttribute("orgName", orgName);
+
+        return "index";
+    }
+
+    @GetMapping("createSlide")
+    public String createSlide(@PathVariable String orgName, Model model){
+        model.addAttribute("orgName", orgName);
+
+        return "createSlide";
+    }
+
+    @GetMapping("editSlide/{id}")
+    public String editSlide(@PathVariable String orgName, @PathVariable int id, Model model){
+        model.addAttribute("orgName", orgName);
+        model.addAttribute("slideId", id);
+
+        return "editSlide";
+    }
+
+    @GetMapping("slideshow")
+    public String slideshow(@PathVariable String orgName, Model model){
+        model.addAttribute("orgName", orgName);
+
+        return "slideshow";
+    }
+
+    @GetMapping("createAccount")
+    public String createAccount(@PathVariable String orgName, Model model){
+        model.addAttribute("orgName", orgName);
+
+        return "createAccount";
+    }
+
+    @GetMapping("t")
+    public String test(@PathVariable String orgName, Model model){
+        model.addAttribute("orgName", orgName);
+
+        return "test";
+    }
+
+
+
+}
