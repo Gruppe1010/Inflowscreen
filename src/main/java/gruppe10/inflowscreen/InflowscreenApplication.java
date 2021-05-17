10d60aeece3e989a2a02ad455cd039252b7c1d98
Vibs <vibej@hotmail.com>
2021-05-17 12:06:02 +0200

    Initial commit

---
+++ src/main/java/gruppe10/inflowscreen/InflowscreenApplication.java
@@ -0,0 +1,13 @@
+package gruppe10.inflowscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflowscreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflowscreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe10/inflowscreen/InflowscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe10.inflowscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflowscreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
