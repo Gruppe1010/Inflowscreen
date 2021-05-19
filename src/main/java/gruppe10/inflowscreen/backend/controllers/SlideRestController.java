ace4b3f8c3c9caeeb8fc47d270609155461622a9
Vibs <vibej@hotmail.com>
2021-05-19 15:54:18 +0200

    Changes by rasmu

--- src/main/java/gruppe10/inflowscreen/backend/controllers/SlideRestController.java
+++ src/main/java/gruppe10/inflowscreen/backend/controllers/SlideRestController.java
@@ -1,6 +1,8 @@
 package gruppe10.inflowscreen.backend.controllers;
 
+import gruppe10.inflowscreen.backend.models.entities.Organisation;
 import gruppe10.inflowscreen.backend.models.entities.Slide;
+import gruppe10.inflowscreen.backend.repositories.OrganisationRepository;
 import gruppe10.inflowscreen.backend.repositories.SlideRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,7 +22,9 @@ public class SlideRestController
     
     @Autowired
     SlideRepository slideRepository;
-    
+    @Autowired
+    OrganisationRepository organisationRepository; //hfh
+    // hej
     
     @PostMapping("/saveSlide")
     public ResponseEntity<HttpStatus> saveSlide(@RequestBody Slide slide, Principal principal)
@@ -29,9 +33,18 @@ public class SlideRestController
         // vi ser om der er et slide med samme titel, som de prøver at oprette
         Optional<Slide> optionalSlide = slideRepository.findByTitle(slide.getTitle());
         
-        if(optionalSlide.isEmpty()){
+        // hvis der IKKE er slide med samme titel
+        if(optionalSlide.isEmpty())
+        {
+            // vi henter organisationen som skal forbindes til slidet
+            Organisation organisation = organisationRepository.findByEmail(principal.getName());
             
-            principal
+            // hvis der endnu ikke er noget org på slidet - lav nyt Set
+            if(slide.getOrganisations() == null) {
+               slide.setOrganisations(new HashSet<>());
+            }
+            // tilføj org til slide
+            slide.getOrganisations().add(organisationRepository.findByEmail(principal.getName()));
             
             slideRepository.save(slide);
             return new ResponseEntity<>(HttpStatus.CREATED);
@@ -39,11 +52,4 @@ public class SlideRestController
         
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
-    
-    
-    
-    
-    
-    
-
 }
