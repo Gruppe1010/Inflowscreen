348b7f01fbc2fa22e525b5912761f275d6f912e4
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-05-19 09:03:11 +0200

    SlideImage oprettet

---
+++ src/main/java/gruppe10/inflowscreen/backend/models/entities/SlideImage.java
@@ -0,0 +1,29 @@
+package gruppe10.inflowscreen.backend.models.entities;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class SlideImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String imagePath;
+    private boolean fullscreen;
+    private double x;
+    private double y;
+
+
+}
