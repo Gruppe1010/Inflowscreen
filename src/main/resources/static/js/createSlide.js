0d0fde0be15c0c7b0624ea210ff6a7b0197562c0
Vibs <vibej@hotmail.com>
2021-05-20 12:42:58 +0200

    i gang med backend til imageForm

--- src/main/resources/static/js/createSlide.js
+++ src/main/resources/static/js/createSlide.js
@@ -24,17 +24,46 @@ const btnFullscreen = document.getElementById("btnFullscreen");
 
 const btnSave = document.getElementById("btnSave");
 
-let title;
+const imageForm = document.getElementById('imageForm');
 
-const url = `http://localhost:8081/saveSlide`;
 
+
+// når der submittes
+imageForm.addEventListener("submit", handleImageSubmit);
 btnSave.addEventListener('click', saveSlide);
 
+// når der trykkes på submit fanger vi dataen fra imageInputfelt og gemmer det på liste
+function handleImageSubmit(){
+
+}
 function saveSlide(){
+    let title;
+    const url = `http://localhost:8081/saveSlide`;
+
+    // TODO tilføj noget async noget så vi ved vi har fået hentet dataen
+    function retrieveInput(){
+        title = inpTitle.value;
+        console.log("Title er her: " + title);
+
+    }
     retrieveInput();
+
+
 
     // hvis de har indtastet en titel
     if(title.length > 0){
+        // TODO tilføj måske noget asyng
+        function createJSONSlide(title){
+            // vi laver et object (JSON-obj er standard obj i js)
+            let slide = {
+                "title": title
+            };
+
+            /* Vi laver JSON om til en String
+               Fordi at body i requestOptions'en skal angives som string
+             */
+            return JSON.stringify(slide);
+        }
         const body = createJSONSlide(title);
 
         const requestOptions = {
@@ -45,6 +74,21 @@ function saveSlide(){
             body: body
         };
 
+        // TODO tilføjet måske noget async
+        function checkIfSuccess(response){
+            if(response.status >= 200 && response.status < 300){
+
+                const cookie = document.cookie;
+
+                alert(cookie);
+
+                window.location.replace("/");
+            }
+            else if(response.status === 409){
+                alert("Der findes allerede et slide med denne title, vælg venligst en anden.");
+            }
+            // else bliver den catchet i fetch
+        }
         fetch(url, requestOptions)
             .then(data => checkIfSuccess(data))
             .catch(error => console.log("Fejl i fetch, createSlide.js: ", error));
@@ -55,42 +99,63 @@ function saveSlide(){
 
 }
 
-function retrieveInput(){
-    title = inpTitle.value;
-    console.log("Title er her: " + title);
 
-}
+// kaldes når man trykker på imageInputfelt - sørger for at submitte billedet
+function imageAutoSubmit(input){
 
-function createJSONSlide(title){
-    // vi laver et object (JSON-obj er standard obj i js)
-    let slide = {
-        "title": title
-    };
+    // hvis der er noget i input-feltet for image
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            document.imageForm.submit();
 
-    /* Vi laver JSON om til en String
-       Fordi at body i requestOptions'en skal angives som string
-     */
-    return JSON.stringify(slide);
-}
+            /* TODO lav så den viser billedet i canvaset
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
 
-function checkIfSuccess(response){
-    if(response.status >= 200 && response.status < 300){
-
-        const cookie = document.cookie;
-
-        alert(cookie);
-
-        window.location.replace("/");
+             */
     }
-    else if(response.status === 409){
-        alert("Der findes allerede et slide med denne title, vælg venligst en anden.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+// til upload af billede
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
     }
-    // else bliver den catchet i fetch
 }
 
-function createTextBox(){
 
-}
+
+
+
+
+
+
 
 
 const canvasContext = canvas.getContext("2d");
@@ -101,6 +166,7 @@ canvasContext.strokeText("Hello World",10,50);
 const canvasContext1 = canvas.getContext("2d");
 canvasContext1.font = "60px Arial";
 canvasContext1.strokeText("Heeeeej",30,90);
+
 
 
 
