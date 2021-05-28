8087be27d0ae6235099f5245c7038eca8bd3aa10
Vibs <vibej@hotmail.com>
2021-05-28 11:02:43 +0200

    print af fetch result

--- src/main/resources/static/js/slideshow.js
+++ src/main/resources/static/js/slideshow.js
@@ -16,7 +16,7 @@ async function getAllSlides() {
     };
 
     // Default options are marked with *
-    return await fetch(url, requestOptions).then(response => console.log(response));//response.json());
+    return await fetch(url, requestOptions).then(response => console.log(response.json()));//response.json());
 
 
 }
