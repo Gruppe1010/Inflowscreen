de8fef1b40156b912ec6b05c2f2db1c40b909777
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-05-31 13:08:44 +0200

    overflow hidden

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,3 +2,50 @@ $( function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
 } );
+
+
+const indexSlide = document.getElementById('sortable');
+
+
+function getAllSlidesByTitle() {
+    //const url = `http://localhost/api/slideshow/${orgId}`; // localhost
+    const url = `http://inflowscreen.dk/api/slides/${orgId}`; // online
+
+
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json', // betyder == vi sender et json i string-format
+        },
+        redirect: 'follow'
+    };
+
+         fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(slides => slides.forEach(showSlides))
+        .catch(error => console.log("error: ", error));
+}
+
+getAllSlidesByTitle();
+
+function showSlides(slides){
+
+    const liIndex = document.createElement('li');
+    const spanIndex = document.createElement('span');
+    const inpIndex = document.createElement('input');
+    const labelIndex = document.createElement('label');
+    labelIndex.innerText = slides.title;
+
+
+
+
+    inpIndex.appendChild(labelIndex);
+    spanIndex.appendChild(inpIndex);
+    liIndex.appendChild(spanIndex);
+    indexSlide.appendChild(liIndex);
+
+
+}
+
+
+
