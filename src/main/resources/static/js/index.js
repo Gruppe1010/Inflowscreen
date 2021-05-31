952ba452c546c917d85fa3e3aa74b66e9e2a379f
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-05-31 13:35:35 +0200

    Index påbegydnt

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,7 +5,7 @@ $( function() {
 
 
 const indexSlide = document.getElementById('sortable');
-
+let inpIndexId = 0;
 
 function getAllSlidesByTitle() {
     const url = `http://localhost/api/slides/${orgId}`; // localhost
@@ -30,10 +30,25 @@ getAllSlidesByTitle();
 
 function showSlides(slides){
 
+    inpIndexId++;
+
     const liIndex = document.createElement('li');
+    liIndex.classList.add("btn", "btn-dark", "li-width", "ui-sortable-handle");
+
+
     const spanIndex = document.createElement('span');
-    const inpIndex = document.createElement('input').type = 'checkbox';
+    spanIndex.classList.add("bi");
+    spanIndex.classList.add("bi-arrow-down-up");
+
+    const inpIndex = document.createElement('input');
+    inpIndex.setAttribute("id", "flexSwitchCheckDefault" + inpIndexId)
+    inpIndex.classList.add("form-check-input");
+    inpIndex.type = 'checkbox';
+    inpIndex.checked = slides.isActive;
+
     const labelIndex = document.createElement('label');
+    labelIndex.classList.add("custom-checkbox");
+    labelIndex.setAttribute('for', "flexSwitchCheckDefault" + inpIndexId)
 
     labelIndex.innerText = slides.title;
 
@@ -44,8 +59,6 @@ function showSlides(slides){
     spanIndex.appendChild(inpIndex);
     liIndex.appendChild(spanIndex);
     indexSlide.appendChild(liIndex);
-
-
 }
 
 
